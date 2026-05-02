65c187dcc3a7f43fa01910548a166b8e1c41e779
mateusofneves <prmateus.ofn@gmail.com>
2026-05-01 22:37:38 -0300

    criando as primeiras funcoes e trazendo referencias as mesmas

--- banco/banco.cpp
+++ banco/banco.cpp
@@ -9,6 +9,28 @@ struct Conta
     float saldo;
 };
 
+void sacar(Conta& conta, float valorASacar)
+{
+    if (valorASacar < 0) {
+        cout << "Nao pode sacar valor negativo." << endl;
+        return;
+    } else if (valorASacar > conta.saldo) {
+        cout << "Saldo insuficiente." << endl;
+        return;
+    }
+    
+    conta.saldo -= valorASacar;
+}
+
+void depositar (Conta& conta, float valorADepositar)
+{
+    if (valorADepositar < 0) {
+        cout << "Nao pode depositar valor negativo." << endl;
+        return;
+    }
+
+    conta.saldo += valorADepositar;
+}
 
 int main() 
 {
@@ -20,6 +42,8 @@ int main()
 
     Conta outraConta;
     outraConta.saldo = 200;
+
+    depositar(outraConta, 500);
 
     cout << "Uma Conta: " << umaConta.saldo << " Outra conta: " << outraConta.saldo << endl;
 
